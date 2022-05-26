ba86d8f4a2db887a31f223362a984e72915efb6b
abrahamlimon404 <95248172+abrahamlimon404@users.noreply.github.com>
2022-05-26 00:13:58 -0700

    Create IP.py

---
+++ IP.py
@@ -0,0 +1,14 @@
+class randomapp:
+    def runThis():
+        # importing socket module
+        import socket
+        ## getting the hostname by socket.gethostname() method
+        hostname = socket.gethostname()
+        ## getting the IP address using socket.gethostbyname() method
+        ip_address = socket.gethostbyname(hostname)
+        ## printing the hostname and ip_address
+        print(f"Hostname: {hostname}")
+        print(f"IP Address: {ip_address}")
+        return ip_address
+
+    print(runThis())
